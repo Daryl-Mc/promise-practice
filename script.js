d8ea32d03739ffd1caa3dae1fd47360b90f0200e
daryl <daryl.mcafee@gmail.com>
2023-08-09 07:32:37 -0400

    Fixed an issue I had with .then and .catch statements and commented out redudnant code.

--- script.js
+++ script.js
@@ -8,51 +8,52 @@ function colorSwitcher(textContent, background, color){
     return new Promise ((resolve, reject) => {
         output.style.background = background;
         switchBtn.textContent = textContent;
-        resolve(`switched to ${color}`)
+        resolve(`Color switched to ${color}`)
     })
 }
 
 
 function switcher(){
     if(green){
-        colorSwitcher("Blue", "blue", "Blue");
-        // .then((message) => {
-        //     console.log(message)
-        // }).catch((error) => {
-        //     console.log(error)
-        // })
+        colorSwitcher("Blue", "blue", "Blue")
+        .then((message) => {
+            console.log(message)
+        }).catch((error) => {
+            console.log(error)
+        })
         green = false;
         blue = true;
     } else if (blue){
         blue = false; 
         red = true;
-        colorSwitcher("Red", "red", "Red" );
-        // .then(() => {
-        //     console.log(message);
-        // }) .catch ((error) => {
-        //     console.log(error);
-        // })
+        colorSwitcher("Red", "red", "Red" )
+        .then((message) => {
+            console.log(message);
+        }) .catch ((error) => {
+            console.log(error);
+        })
     } else{
         green = true; 
         red = false;
-        colorSwitcher("Green", "green", "Green");
-        // .then((message) => () {
-        //     console.log(message)
-        // }) .catch((error) => {
-        //     console.log(error)
-        // })
+        colorSwitcher("Green", "green", "Green")
+        .then((message) => {
+            console.log(message)
+        }) .catch((error) => {
+            console.log(error)
+        })
     }
-    return new Promise ((resolve, reject) =>{
-        if (blue){
-            resolve("blue");
-        }if (red){
-            reject("red")
-        }if (green){
-            resolve("green")
-        }
-    }).then((message) => {
-        console.log("The button is " + message);
-    }).catch((message) => {
-        console.log("Oh no its " + message)
-    })
 }
+//     return new Promise ((resolve, reject) =>{
+//         if (blue){
+//             resolve("blue");
+//         }if (red){
+//             reject("red")
+//         }if (green){
+//             resolve("green")
+//         }
+//     }).then((message) => {
+//         console.log("The button is " + message);
+//     }).catch((message) => {
+//         console.log("Oh no its " + message)
+//     })
+// 
